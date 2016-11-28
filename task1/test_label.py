0d6f6f4db341fe43e76886e1f2922fe1b2beff5b
Alona Stepanenko <stepanchik.da02@gmail.com>
2016-11-29 00:23:14 +0200

    task8

--- task1/test_label.py
+++ task1/test_label.py
@@ -18,14 +18,12 @@ def driver(request):
 
 def test_menu(driver):
     driver.get("http://localhost/litecart/en/")
-    #driver.find_element_by_name("username").send_keys("admin")
-    #driver.find_element_by_name("password").send_keys("admin")
-    #driver.find_element_by_name("login").click()
     WebDriverWait(driver, 2).until(EC.title_is(u"Online Store | My Store"))
 
     products = driver.find_elements_by_class_name('product')
     assert len(products) > 0
     for product in products:
         assert len(product.find_elements_by_class_name('sticker')) == 1
-    
+
+
 
